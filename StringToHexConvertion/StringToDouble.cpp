7c8957adf86d087bf6de701f60c53a0cc7bebae3
Dutchy <63201809+avangersKinkyClient@users.noreply.github.com>
2021-03-21 19:24:20 +0100

    Converting the content of a char array or a string to a double variable

--- StringToHexConvertion/StringToDouble.cpp
+++ StringToHexConvertion/StringToDouble.cpp
@@ -1,4 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+
 int main()
 {
-
+	// Converting the content of a char array or a string to a double variable
+	double Double;
+	std::string string;
+	string = "4.5";
+	std::istringstream(string) >> Double;
+	std::cout << "\nThe value of the double variable Double is " << Double << std::endl;
+	std::istringstream("9.87654") >> Double;
 }
