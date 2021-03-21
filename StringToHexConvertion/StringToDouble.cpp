270995674947d27e3c38227fe03d32104476cc08
Dutchy <63201809+avangersKinkyClient@users.noreply.github.com>
2021-03-21 20:37:23 +0100

    Converting a double to string with formatting restrictions | finished

--- StringToHexConvertion/StringToDouble.cpp
+++ StringToHexConvertion/StringToDouble.cpp
@@ -11,4 +11,15 @@ int main()
 	std::istringstream(string) >> Double;
 	std::cout << "\nThe value of the double variable Double is " << Double << std::endl;
 	std::istringstream("9.87654") >> Double;
+
+	// Converting a double to string with formatting restrictions
+	double dble = 3.771234567;
+	std::ostringstream osStream;
+	osStream.fill('#');
+	osStream << "<<<" << std::setprecision(6) << std::setw(20) << dble << ">>>";
+	
+	string = osStream.str(); // formatted converted double is now in string
+	std::cout << "\nThe value of the string variable string is " << string << std::endl;
+
+	return 0;
 }
